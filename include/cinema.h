3ca7630a9ea671be5609908a2b4d1bc1df08b231
Amine El harrab <amineelharrab.cyber@gmail.com>
2025-06-18 21:31:44 +0100

    Update cinema.h

--- include/cinema.h
+++ include/cinema.h
@@ -21,4 +21,4 @@ extern salle * reservation_seige (salle * tab) ;
 extern salle * annulation_reservation (salle* tab) ;
 extern void  Affichage_salles(salle* tab) ;
 extern void Affichage_reservateur();
-#endif // EX_H_1_H_INCLUDED
+#endif // CINEMA_H_INCLUDED
